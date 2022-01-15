02f62613c685ad563d1a32e7f7494f378a35caaa
sandengjiang <577079879@qq.com>
2022-01-15 08:24:27 +0800

    second commit

--- src/main/java/com/atzhongyiyao/Demo1.java
+++ src/main/java/com/atzhongyiyao/Demo1.java
@@ -7,5 +7,6 @@ package com.atzhongyiyao;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("lalall");
+        System.out.println("lalalala");
     }
 }
