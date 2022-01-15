ff278b34f3eb7cd64769a1686d0ce6849cf4bd86
sandengjiang <577079879@qq.com>
2022-01-15 08:41:52 +0800

    hot-fix commit2

--- src/main/java/com/atzhongyiyao/Demo1.java
+++ src/main/java/com/atzhongyiyao/Demo1.java
@@ -10,6 +10,7 @@ public class Demo1 {
         System.out.println("lalalala");
         System.out.println("lalalalalal");
         System.out.println("hot-fix 11111");
+        System.out.println("hot-fix 22222");
 
     }
 }
