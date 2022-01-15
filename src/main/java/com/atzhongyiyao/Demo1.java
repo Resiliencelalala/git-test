14b27d5ee44097956c9a9a9a22356c8b89b50480
sandengjiang <577079879@qq.com>
2022-01-15 10:02:40 +0800

    hot-fix冲突测试1

--- src/main/java/com/atzhongyiyao/Demo1.java
+++ src/main/java/com/atzhongyiyao/Demo1.java
@@ -11,6 +11,7 @@ public class Demo1 {
         System.out.println("lalalalalal");
         System.out.println("hot-fix 11111");
         System.out.println("aaaaa");
+        System.out.println("ccccc");
         System.out.println("gitee test");
     }
 }
