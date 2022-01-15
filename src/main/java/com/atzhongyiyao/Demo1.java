1290fe7c05325ae22e6c62ad187b8742368cdfea
sandengjiang <577079879@qq.com>
2022-01-15 08:21:25 +0800

    first commit

---
+++ src/main/java/com/atzhongyiyao/Demo1.java
@@ -0,0 +1,11 @@
+package com.atzhongyiyao;
+
+/**
+ * author:ZH
+ * date:2022/1/15 8:16
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("lalall");
+    }
+}
