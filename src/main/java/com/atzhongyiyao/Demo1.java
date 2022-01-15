0066ceede0bf2b8e6eb2a97d9fcb5643cd220ae6
sandengjiang <577079879@qq.com>
2022-01-15 09:50:44 +0800

    冲突测试

--- src/main/java/com/atzhongyiyao/Demo1.java
+++ src/main/java/com/atzhongyiyao/Demo1.java
@@ -13,7 +13,7 @@ public class Demo1 {
         System.out.println("hot-fix 22222");
         System.out.println("push test");
         System.out.println("pull test");
-        System.out.println("pull test");
+        System.out.println("gitee test");
         System.out.println("gitee test");
     }
 }
